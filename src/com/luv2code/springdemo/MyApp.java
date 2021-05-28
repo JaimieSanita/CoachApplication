cd73f4cbe5ca57277ec46bbabdc02ebecd91f305
Jaimie Sanita <jsanita11@gmail.com>
2021-05-28 11:10:01 -0400

    added soccer coach

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -4,9 +4,11 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		//create the object; coding to the interface 
-		Coach thecoach = new TrackCoach();
+		Coach theCoach = new TrackCoach();
+		Coach secondCoach = new SoccerCoach();
 		//use the object
-		System.out.println(thecoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(secondCoach.getDailyWorkout());
 		
 		
 	}
