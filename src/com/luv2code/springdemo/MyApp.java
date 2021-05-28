0ebe79a2cab3edfe79e4335dc64eed97f8eaa0ed
Jaimie Sanita <jsanita11@gmail.com>
2021-05-28 11:38:56 -0400

    configured xml bean

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		//create the object; coding to the interface 
-		Coach theCoach = new TrackCoach();
+		Coach theCoach = new TrackCoach(); 
 		Coach secondCoach = new SoccerCoach();
 		//use the object
 		System.out.println(theCoach.getDailyWorkout());
