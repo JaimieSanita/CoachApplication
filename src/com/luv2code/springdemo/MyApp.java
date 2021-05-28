0652a8a13fa0b8383c62dde604194f729595d428
Jaimie Sanita <jsanita11@gmail.com>
2021-05-28 15:37:44 -0400

    setter injections with xml file

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -5,10 +5,9 @@ public class MyApp {
 	public static void main(String[] args) {
 		//create the object; coding to the interface 
 		Coach theCoach = new TrackCoach(); 
-		Coach secondCoach = new SoccerCoach();
 		//use the object
 		System.out.println(theCoach.getDailyWorkout());
-		System.out.println(secondCoach.getDailyWorkout());
+		
 		
 		
 	}
--- src/com/luv2code/springdemo/SoccerCoach.java
+++ src/com/luv2code/springdemo/SoccerCoach.java
@@ -1,17 +1,28 @@
 package com.luv2code.springdemo;
 
 public class SoccerCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	//create no arg constructor for spring to call when making ref to bean
+	public SoccerCoach() {
+		System.out.println("SoccerCoach: inside no-arg constrcutor.");
+	}
+	
+	//setter method to be called by spring with dependency injected
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("SoccerCoach: inside setter method - setFortuneService.");
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
-		
-		return "Bend it like Beckham!";
+		return "Dribble quickly for 15 minutes!";
 	}
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
